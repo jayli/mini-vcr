aa443dd4e726360409db29e87f82fa65b1e12cc4
lijing00333 <lijing00333@163.com>
2024-09-10 07:55:25 +0800

    bugfix

--- config/config.ts
+++ config/config.ts
@@ -3,6 +3,6 @@ export default {
   mock: {
     include: ['api/*.ts', 'api/*.js']
   },
-  plugins: ['@umijs/plugins/dist/react-query'],
-  reactQuery: {},
+  // plugins: ['@umijs/plugins/dist/react-query'],
+  // reactQuery: {},
 };
